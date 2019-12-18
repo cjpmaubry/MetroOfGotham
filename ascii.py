2fc70127484c3ddae098779ff420f7de5771dece
Leoduv <leopold.duvergernoinski@edu.devinci.fr>
2019-12-18 17:53:40 +0100

    final working version

--- ascii.py
+++ ascii.py
@@ -1,5 +1,5 @@
 
-
+#Returns the ascii value of the string "name"
 def FromNameToAscii(name):
     crypted_name = 0
     offset = 1000
@@ -11,22 +11,27 @@ def FromNameToAscii(name):
             position += 1
     return crypted_name
 
+#Returns the string of the "crypted" ascii 
 def FromAsciiToName(crypted):
     name = ""
     offset = 1000
     index = 0
+    #finds the length of the word to recreate
     if len(str(crypted)) % 3 == 0:
         number_letters = int(len(str(crypted)) / 3)
     else:
         number_letters = int((len(str(crypted)) + 1) / 3)
 
+    #adds each letter to the name
     for index in range(number_letters):
-        temp = int(crypted / pow(offset, number_letters - index - 1))
-        crypted = crypted - (temp * pow(offset, number_letters - index - 1))
-        name += chr(temp % pow(offset, number_letters - index))
+        letter = int(crypted / pow(offset, number_letters - index - 1))
+        #"substracts" the letter once its ascii value has been stored in "letter"
+        crypted -= letter * pow(offset, number_letters - index - 1)
+        #adds that letter to the word
+        name += chr(letter % pow(offset, number_letters - index))
         index += 1
     return name
 
-test = FromNameToAscii("leopold")
+test = FromNameToAscii("agathe")
 
 print(FromAsciiToName(test))
