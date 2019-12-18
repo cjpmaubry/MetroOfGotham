390ae64def76b06a1c5883d74b494f0c129465e9
Leoduv <leopold.duvergernoinski@edu.devinci.fr>
2019-12-18 12:14:35 +0100

    prim

--- prims.py
+++ prims.py
@@ -7,9 +7,7 @@ import matplotlib.pyplot as plt
 
 # ----------------------------------------------------------------- PRIM'S ALGORITHM ------------------------------------------------------
 
-# A Python program for Prim's Minimum Spanning Tree (MST) algorithm.
-# The program is for adjacency matrix representation of the graph
-# Part of Cosmos by OpenGenus Foundation
+#Prim's Minimum Spanning Tree algorithm
 class PrimsAlgorithm():
 
     def __init__(self, vertices):
@@ -17,19 +15,20 @@ class PrimsAlgorithm():
         self.graph = [[0 for column in range(vertices)] 
                       for row in range(vertices)]
 
-    # Function to print the constructed MST stored in parent[]
-    def printMST(self, parent):
+    # Builds and returns the matrix associated with the constructed MST stored in "parent"
+    def matrixMST(self, parent):
         results=np.zeros((10,10))
         print ("Edge \tWeight")
         for i in range(1,self.V):
+            #Also prints solution in console
             print (parent[i],"-",i,"\t",self.graph[i][ parent[i] ])
             results[i][parent[i]] = self.graph[i][parent[i]]
         return results
 
-    # Function to find the vertex with minimum distance value, from
-    # the set of vertices not yet included in shortest path tree
+    # Finds the vertex with minimum distance value    
+    # Searches for it in the set of vertices not yet included in shortest path tree
     def minKey(self, key, mstSet):
-        # Initilaize min value
+        #Initilaizes min value wiht big number
         min = 1000000
         for v in range(self.V):
             if key[v] < min and mstSet[v] == False:
@@ -37,32 +36,34 @@ class PrimsAlgorithm():
                 min_index = v
         return min_index
 
-    # Function to construct and print MST for a graph represented using
-    # adjacency matrix representation
+    # Function to construct and print MST for a graph 
+    # uses adjacency matrix representation
     def primMST(self):
         #Key values used to pick minimum weight edge in cut
         key = [1000000] * self.V
-        parent = [None] * self.V # Array to store constructed MST
-        key[0] = 0   # Make key 0 so that this vertex is picked as first vertex
+        parent = [None] * self.V # Stored constructed MST goes here
+        key[0] = 0   #  0 is picked as first vertex
         mstSet = [False] * self.V
-        parent[0] = -1  # First node is always the root of
+        parent[0] = -1  # First node is always the root
+
         for cout in range(self.V):
-            # Pick the minimum distance vertex from the set of vertices not
-            # yet processed. u is always equal to src in first iteration
+            # Picks the minimum distance vertex from the set of vertices not yet processed
             u = self.minKey(key, mstSet)
-            # Put the minimum distance vertex in the shortest path tree
+
+            # Puts the minimum in the shortest path tree
             mstSet[u] = True
-            # Update dist value of the adjacent vertices of the picked vertex
+
+            # Updates dist value of the adjacent vertices of the picked vertex
             # only if the current distance is greater than new distance and
             # the vertex in not in the shotest path tree
             for v in range(self.V):
                 # graph[u][v] is non zero only for adjacent vertices of m
                 # mstSet[v] is false for vertices not yet included in MST
-                # Update the key only if graph[u][v] is smaller than key[v]
+                # Updates the key only if graph[u][v] is smaller than key[v]
                 if self.graph[u][v] > 0 and mstSet[v] == False and key[v] > self.graph[u][v]:
                         key[v] = self.graph[u][v]
                         parent[v] = u
-        return self.printMST(parent)
+        return self.matrixMST(parent)
 
 
 # -------------------------------- DISPLAY -------------------------------- #
