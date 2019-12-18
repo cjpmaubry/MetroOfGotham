46235fae33d9be3426ed56e0ee398ee361a0026a
cjpmaubry <cjpm.aubry@gmail.com>
2019-12-18 13:53:20 +0100

    Added part4 (Part4.py)

---
+++ Part4.py
@@ -0,0 +1,62 @@
+
+
+
+#---------------STRUCTURE OF BINARY TREE--------------#
+
+class Node(object):
+    def __init__(self,value):
+        self.value = value
+        self.left = None
+        self.right= None
+
+class BinaryTree(object):
+    def __init__(self,root):
+        self.root=Node(root)
+
+
+#------------DISPLAY METHOD OF BINARY TREE------------#
+
+    def print_preodrer(self,start):
+        if (start is not None) :
+            print(start.value)
+            self.print_preodrer(start.left)
+            self.print_preodrer(start.right)
+
+    def print_intodrer(self,start):
+        if (start is not None) :
+            self.print_intodrer(start.left)
+            print(start.value)
+            self.print_intodrer(start.right)
+
+    def print_postodrer(self,start):
+        if (start is not None) :
+            self.print_postodrer(start.left)
+            self.print_postodrer(start.right)
+            print(start.value)
+
+#------------UTILITARY METHOD OF BINARY TREE------------#
+
+    def size(self,start):
+        if(start is None):
+            return 0
+        else:
+            return 1+size(start.left)+size(start.right)
+
+    def max_Depth(self,start):
+        if(start is None):
+            return 0
+        else:
+            lDepth = tree.max_Depth(start.left)
+            rDepth = tree.max_Depth(start.right)
+        if (lDepth > rDepth):
+            return(lDepth + 1)
+        else:
+            return(rDepth + 1)
+
+    def search(self,root,key):
+        if root is None or root.value==key:
+            return root
+        if root.value<key:
+            return self.search(root.right,key)
+        return self.search(root.left,key)
+
