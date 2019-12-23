bad8307f43bc5dafae21a4d16bc1511b2609f614
cjpmaubry <cjpm.aubry@gmail.com>
2019-12-23 08:29:02 +0100

    Added AVL methods

--- Part4.py
+++ Part4.py
@@ -140,6 +140,110 @@ def SearchName(tree,name):
         print(FromAsciiToName(node.value))
 
 
+#----------------------------------QUESTION--AVL--------------------------------------#
+
+    def height(self,root):
+        if root is None:
+            return 0
+        return max(self.height(root.left),self.height(root.right))+1
+
+
+    def isBalanced(self,root):
+        if root is None:
+            return True
+        lh=self.height(root.left)
+        rh=self.height(root.right)
+        if(abs(lh-rh)<=1)and self.isBalanced(root.right) is True and self.isBalanced(root.left) is True:
+            return True
+        return False
+
+
+    def insert_AVL(self,root,key):
+
+        if not root:
+            return TreeNode(key)
+        elif key < root.val:
+            root.left=self.insert_AVL(root.left,key)
+        else:
+            root.right=self.insert_AVL(root.right,key)
+        # Update the height of the ancestor node
+        root.height=1+max(self.getHeight(root.left),self.getHeight(root.right))
+        # Get the balance factor
+        balance=self.getBalance(root)
+
+        # If the node is unbalanced, then try out 4 cases
+        #Case 1 - Left Left
+        if balance >1 and key <root.left.val:
+            return self.rightRotate(root)
+        #Case 2 - Right Right
+        if balance <-1 and key >root.right.val:
+            return self.leftRotate(root)
+        #Case 3 - Left Right
+        if balance >1 and key>root.left.val:
+            root.left=self.leftRotate(root.left)
+            return self.rightRotate(root)
+        #Case 4 - Right Left
+        if balance <-1 and key<root.right.val:
+            root.right=self.rightRotate(root.right)
+            return self.leftRotate(root)
+
+        return root
+
+
+
+    def getBalance(self,root):
+        if not root:
+            return 0
+        return self.getHeight(root.left) - self.getHeight(root.right)
+
+
+    def getHeight(self,root):
+        if not root:
+            return 0
+        return root.height
+
+
+    def LeftRotate(self,z):
+
+        y=z.right
+        T2=y.left
+
+        #Perform rotation
+        y.left=z
+        z.right=T2
+
+        #update heights
+        z.height=1+max(self.getHeight(z.left),self.getHeight(z.right))
+        y.height=1+max(self.getHeight(y.left),self.getHeight(y.right))
+
+        #return the new root
+        return y
+
+
+
+    def RightRotate(self,z):
+
+        y=z.left
+        T2=y.right
+
+        #Perform rotation
+        y.right=z
+        z.left=T2
+
+        #update heights
+        z.height=1+max(self.getHeight(z.left),self.getHeight(z.right))
+        y.height=1+max(self.getHeight(y.left),self.getHeight(y.right))
+
+        #return the new root
+        return y
+
+
+
+
+
+
+#-----------------------------------------MAIN-----------------------------------------#
+
 def main():
     dataset=GiveDataSetMembers()
     tree=CreateTree(dataset)
